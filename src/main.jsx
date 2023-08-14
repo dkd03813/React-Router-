54a9f9f1d5eb34ddc3270419c7efb868aa96c2d6
Damian Diaz <damiandiaz0509@gmail.com>
2023-08-13 20:00:03 -0400

    made changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact, {loader as contactLoader} from "./routes/contact";
+import Contact, {loader as contactLoader, action as contactAction} from "./routes/contact";
 import EditContact, {action as editAction} from "./routes/edit";
 import { updateContact } from "./contacts";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
+
 
 const router = createBrowserRouter([
   {
@@ -17,16 +20,26 @@ const router = createBrowserRouter([
     action: rootAction,
     children: [
       {
+        index: true,
+        element: <Index/>
+      },
+      {
         path: "contacts/:contactId",
         element: <Contact />,
         loader: contactLoader,
+        action: contactAction
       },
       {
         path: "contacts/:contactId/edit",
         element: <EditContact/>,
         loader:contactLoader,
         action: editAction,
-      }
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>OH HELL NO</div>
+      },
     ],
   },
 ]);
